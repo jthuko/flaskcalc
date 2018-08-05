bb6187bc9a321bc5a1615d9191c796a0c4490f83
johnthuko <johnthuko@gmail.com>
2018-08-05 03:33:44 -0500

    added comments

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -1,28 +1,23 @@
+
 from flask import Flask, render_template, request
 from sympy import *
 from numpy import *
 
-
+# create app
 app = Flask(__name__,template_folder="templates")
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])# Pull user's input
 def new():
     if request.method == 'POST':
         expression = request.form.get('expression')
-        result = simplify(expression)
+        result = simplify(expression)# Calculate the user input using sympy
         result1 = simplify(expression)
     else:
         result1 = None
 
-    return render_template('index.html', result1=result1)
+    return render_template('index.html', result1=result1)#render results to the user
 
-@app.route("/MathForum")
-def mathforum():
-
-    return render_template('MathForum.html')
-
-
-
+#run app
 if __name__ == "__main__":
  app.run()
